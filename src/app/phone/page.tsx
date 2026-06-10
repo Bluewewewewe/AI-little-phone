1245462b8ffe32d0e5ecc6453807b1dc9f6e6b16
user2215144251 <1646449613868272-user2215144251@noreply.coze.cn>
2026-06-10 17:02:42 +0800

    revert: 恢复爸妈状态为Emoji版本，移除Q版角色动画
    Coze-Commit-Type: user
    Coze-User-ID: 1646449613868272
    Coze-Conversation-ID: 6394436

--- src/app/phone/page.tsx
+++ src/app/phone/page.tsx
@@ -3140,7 +3140,7 @@ export default function PhonePage() {
               {/* Parent Widgets */}
               <div className="parent-widgets">
                 <div className="parent-widget">
-                    <img src="/status_full.png" className="parent-avatar-bob" alt="" />
+                  <span className="parent-emoji">👨</span>
                     <div className="parent-info">
                       <span className="parent-name">{unlockState.unlocked ? '爸爸' : DEFAULT_NAMES.dad1}</span>
                       <span className="parent-status">{parentStatus.dadStatus}</span>
